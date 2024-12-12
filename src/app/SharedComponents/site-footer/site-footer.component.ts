4955fe4e17cb499e3fca5e4572bb803b00676a2a
[firstname <dryekta1979.developer@gmail.com>
2024-12-12 13:47:11 +0330

    Eshop-Pro Angular Project

---
+++ src/app/SharedComponents/site-footer/site-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-footer',
+  imports: [],
+  templateUrl: './site-footer.component.html',
+  styleUrl: './site-footer.component.css'
+})
+export class SiteFooterComponent {
+
+}
